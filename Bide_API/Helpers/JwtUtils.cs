7bfaa8cbd9ab574a4688ec0da46249b340cc1d7c
Tita Liu <tita.liu@titansoft.com.sg>
2024-06-21 15:51:00 +0800

    computer bye

--- Bide_API/Helpers/JwtUtils.cs
+++ Bide_API/Helpers/JwtUtils.cs
@@ -1,5 +1,9 @@
 namespace DefaultNamespace;
 
 public class JwtUtils {
-    
+    private static string secret = "3YhdUIRL776weTA59MSIiekdlLWIyrhTjl";
+
+    public static string GenerateJwtToken(User user) {
+        
+    }
 }
