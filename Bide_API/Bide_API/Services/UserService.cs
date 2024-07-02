2e4c0661695b23d138ff3459242008a6ac443d17
tita <tiffanyliu1224@gmail.com.tw>
2024-07-03 00:58:45 +0800

    password verify complete. need test

--- Bide_API/Bide_API/Services/UserService.cs
+++ Bide_API/Bide_API/Services/UserService.cs
@@ -1,5 +1,6 @@
 using Bide_API.Models;
 using Bide_API.Repositories;
+using Scrypt;
 
 namespace Bide_API.Services;
 
@@ -11,9 +12,11 @@ public class UserService: IUserService {
     }
 
     public async Task<LoginInfo> Login(string userId, string password) {
+        var hashedPassword = HashPassword(password);
+        
         var loginResult = new LoginInfo {
             userId = userId,
-            message = await IsLoginSuccess(userId, password),
+            message = await IsLoginSuccess(userId, hashedPassword),
             userName = ""
         };
         if (loginResult.message != "Success") {
@@ -23,15 +26,30 @@ public class UserService: IUserService {
         loginResult.userName = await GetUserName(userId);
         return loginResult;
     }
+    
+    private static string HashPassword(string password)
+    {
+        var encoder = new ScryptEncoder();
+        
+        var hashedPassword = encoder.Encode(password);
 
-    private async Task<string> IsLoginSuccess(string userId, string password) {
+        return hashedPassword;
+    }
+    
+    static bool VerifyPassword(string password, string correctPassword)
+    {
+        ScryptEncoder encoder = new ScryptEncoder();
+        return encoder.Compare(password, correctPassword);
+    }
+
+    private async Task<string> IsLoginSuccess(string userId, string hashedPassword) {
         var userInfo = await _userRepo.GetUserInfo(userId);
         var userInfoList = userInfo.ToList();
         if (userInfoList.Count == 0) {
             return "User ID is not exist.";
         }
 
-        return password != userInfoList.First().password ? "Password is wrong." : "Success";
+        return VerifyPassword(hashedPassword, userInfoList.First().password) ? "Password is wrong." : "Success";
     }
 
     private async Task<string> GetUserName(string userId) {
